5002144a4206f96f04d135b20ab525364011edc5
japesh5579 <japeshjhatta634@gmail.com>
2024-12-02 21:20:51 +0530

    Update settings.py

--- lms_api/lms_api/settings.py
+++ lms_api/lms_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cu8!$g^bq+164wwbb&z+@^b=c7x!p_pva#!z6khp**dwm@6f36
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.226.33.160', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
