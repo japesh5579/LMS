c7c426e96d80d8af136a39e039c05691905229ed
japesh5579 <japeshjhatta634@gmail.com>
2024-12-03 03:33:20 +0530

    Update settings.py

--- lms_api/lms_api/settings.py
+++ lms_api/lms_api/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles)
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URLS ='/media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
 
