e161a7fb1ee3593ab4b832e0936d0a1edfe732c8
japesh5579 <japeshjhatta634@gmail.com>
2024-12-03 03:37:10 +0530

    Update settings.py

--- lms_api/lms_api/settings.py
+++ lms_api/lms_api/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URLS ='/media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
